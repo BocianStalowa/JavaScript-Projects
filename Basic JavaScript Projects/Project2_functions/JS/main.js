61200f36feefb67923b0ddfc2a76d477d352a1eb
BocianStalowa <112272623+BocianStalowa@users.noreply.github.com>
2022-10-26 16:30:25 +0100

    Assignment Submission # 2

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,5 @@
+function concatenate() {
+    var sentence = "This is "; //assign variable
+    sentence += "Javascript!" // add another text using += operators
+    document.getElementById("concat").innerHTML = sentence // show concatenated text in paragraph
+}
